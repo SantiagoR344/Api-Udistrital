e639b2c466c9c1a12c54466cbf3f0b8b603cd0ef
Santiago Ramos <santiramos0630@gmail.com>
2023-06-20 23:01:33 -0500

    add styes and artist list

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //fetch api
-const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=q&per_page=10&page=1';
+const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/?q=title&per_page=15&page=1';
 const options = {
 	method: 'GET',
 	headers: {
@@ -13,9 +13,49 @@ async function fetchdata(){
         const response = await fetch(url, options);
         const data = await response.json();
         console.log(data);
+        
     } catch (error) {
         console.error("Error al consumir api",error);
     }
-
+    
     }
-fetchdata();
+
+
+// async function main(){
+//     const apiData = await fetchdata();
+
+//     apiData.forEach(element => {
+//     const songTitle = element.hits;
+//     console.log(songTitle);
+// });
+// };
+// main();
+//create funtion to get data search son from api and shaw in html
+function getSong(){
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        
+        const songTitle = data.hits;
+        console.log(songTitle);
+        songTitle.forEach(element => {
+            const songTitle = element.result.full_title;
+            const songArtis = element.result.artist_names;
+            const backgroundImage = element.result.header_image_thumbnail_url;
+            console.log(songTitle);
+            const content = document.getElementById('TablesongTitle');
+            const songArtisElement = document.createElement('label') ;
+            const songTitleElement = document.createElement('p');
+            songArtisElement.textContent = songArtis;
+            songTitleElement.textContent = songTitle;
+            const divElement = document.createElement('div');
+            divElement.id = 'songArtis';
+            divElement.title = 'songTitles';
+            divElement.style.backgroundImage = `url(${backgroundImage})`;
+            divElement.appendChild(songArtisElement);
+            divElement.appendChild(songTitleElement);
+            document.body.appendChild(content).appendChild(divElement);
+        });
+    });
+}getSong();
+
